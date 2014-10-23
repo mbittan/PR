7a5e098055d8c79ee0bb6b3030bf8affc83cb8db
Redha Gouicem <redha.gouicem@gmail.com>
2014-10-23 20:38:24 +0200

    added closing shared mem for client

--- TME5/src/chat_client.c
+++ TME5/src/chat_client.c
@@ -17,37 +17,56 @@ typedef struct request {
     char content[1024];
 } req_t;
 
-req_t messages[NBMSG];
-int curs;
-
+req_t *message;
 
 int main(int argc, char ** argv){
+  char * clientid=argv[1];
+  char * serverid=argv[2];
+  char clientshmname[1031];
+  int shm_id;
+
   if(argc!=3){
     fprintf(stderr,"Nombre d'arguments incorrect\n");
     fprintf(stderr,"Usage : %s <client_id> <server_id>",argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  char * clientid=argv[1];
-  char * serverid=argv[2];
-  char clientshmname[1031];
-  int shm_id;
-
+  // build shared memory name
   sprintf(clientshmname,"/%s_shm:0",clientid);
 
-  curs = 0;
-
+  /***********  Initializing shared memory segment  ***********/
+  
   if((shm_id = shm_open(clientshmname, O_CREAT | O_RDWR, 0666)) == -1){
     perror("shm_open");
     exit(EXIT_FAILURE);
   }
 
-  if(ftruncate(shm_id, NBMSG*sizeof(req_t)) == -1){
+  if(ftruncate(shm_id, sizeof(req_t)) == -1){
     perror("ftruncate");
     exit(EXIT_FAILURE);
   }
 
-  if(
+  if((message = mmap(NULL, sizeof(req_t), PROT_READ | PROT_WRITE, MAP_SHARED,
+		     shm_id, 0)) == MAP_FAILED){
+    perror("mmap");
+    exit(EXIT_FAILURE);
+  }
+
+  /*******************  Infinite loop  *******************/
+
+  
+
+  /***********  Closing shared memory segment  ***********/
+
+  if(munmap(message, sizeof(req_t)) == -1){
+    perror("munmap");
+    exit(EXIT_FAILURE);
+  }
+
+  if(shm_unlink(clientshmname) == -1){
+    perror("shm_unlink");
+    exit(EXIT_FAILURE);
+  }
 
   return EXIT_SUCCESS;
 }
