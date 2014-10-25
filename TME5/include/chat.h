47a26bfaf19bcd7d46bcf49975bec6b6a6c79612
Redha Gouicem <redha.gouicem@gmail.com>
2014-10-25 23:42:49 +0200

    nouvelles structures (semaphores en shm)

--- TME5/include/chat.h
+++ TME5/include/chat.h
@@ -1,9 +1,20 @@
 #ifndef CHAT_H_
 #define CHAT_H_
 
+#define BUFSZ 1024
+
+#define CONNECT 0
+#define DIFF 1
+#define DISCONNECT 2
+
 typedef struct request {
   long type;
-  char content[1024];
+  char content[BUFSZ];
 } req_t;
+
+typedef struct shm {
+  req_t msg;
+  sem_t sem;
+} shm_t;
 
 #endif
