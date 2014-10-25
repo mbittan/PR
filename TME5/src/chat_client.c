cecb5a240a8dc59065dc573e2c04952895e15c10
Redha Gouicem <redha.gouicem@gmail.com>
2014-10-25 23:40:58 +0200

    client fini (a tester)

--- TME5/src/chat_client.c
+++ TME5/src/chat_client.c
@@ -6,56 +6,104 @@
 #include <sys/mman.h>
 #include <time.h>
 #include <sys/stat.h>
+#include <signal.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <wait.h>
+#include <string.h>
 #include <semaphore.h>
 
 #include "chat.h"
 
-req_t *message;
-sem_t *sem;
+char *clientid;
+char clientshmname[BUFSZ+7];
+char servershmname[BUFSZ+7];
+shm_t *shm;
+shm_t *servershm;
 char buf[BUFSZ];
 
 void echo_loop() {
   int c;
+
+  /**********  Connect to server ***********/
+
+  sem_wait(&servershm->sem);
+  (servershm->msg).type = CONNECT;
+  strncpy((servershm->msg).content, clientid, BUFSZ);
+  sem_post(&servershm->sem);
+
+  /**********  Send/receive loop  **********/
+
   while (1) {
     if((c = read(STDIN_FILENO, buf, BUFSZ)) == -1){
       perror("read");
       exit(EXIT_FAILURE);
     }
-    if(c>0)
-      sendMessageToServer();
-    if(buf[0] != '\0'){
-      printf("%s\n", buf);
-      buf[0] = '\0';
+    if(c>0){
+      sem_wait(&servershm->sem);
+      (servershm->msg).type = DIFF;
+      strncpy((servershm->msg).content, buf, BUFSZ);
+      sem_post(&servershm->sem);
     }
-    usleep(100000);
+    if((shm->msg).type == 1){
+      printf("%s\n", (shm->msg).content);
+      (shm->msg).type = -1;
+    }
+    if((shm->msg).type == 2){
+      kill(getpid(), SIGUSR1);
+    }
+    sleep(1);
   }
 }
 
+void sighandler(int sig){
+  if(sig != SIGUSR1){
+  /*********  Send disconnect message to server  *********/
+    sem_wait(&servershm->sem);
+    (servershm->msg).type = DISCONNECT;
+    strcpy((servershm->msg).content, clientid);
+    sem_post(&servershm->sem);
+  }
+
+  /*****************  Closing semaphore  *****************/
+  
+  if(sem_destroy(&(shm->sem)) == -1){
+    perror("sem_destroy");
+    exit(EXIT_FAILURE);
+  }
+
+  /***********  Closing shared memory segment  ***********/
+
+  if((munmap(shm, sizeof(shm_t)) == -1) ||
+     (munmap(servershm, sizeof(shm_t)) == -1)){
+    perror("munmap");
+    exit(EXIT_FAILURE);
+  }
+
+  if((shm_unlink(clientshmname) == -1) ||
+     (shm_unlink(servershmname) == -1)){
+    perror("shm_unlink");
+    exit(EXIT_FAILURE);
+  }
+  
+}
+
 int main(int argc, char ** argv){
-  char *clientid=argv[1];
-  char *serverid=argv[2];
-  char clientshmname[1031];
-  char clientsemname[1031];
-  int shm_id;
+  char *serverid;
+  int shm_id, server_shm_id;
+  struct sigaction act;
 
   if(argc!=3){
     fprintf(stderr,"Nombre d'arguments incorrect\n");
     fprintf(stderr,"Usage : %s <client_id> <server_id>",argv[0]);
     exit(EXIT_FAILURE);
   }
-
-  // build shared memory and semaphore names
+  
+  clientid=argv[1];
+  serverid=argv[2];
+  // build shared memory names
   sprintf(clientshmname,"/%s_shm:0",clientid);
-  sprintf(clientsemname,"/%s_sem:0",clientid);
-
-  /*****************  Initializing semaphore  *****************/
-
-  if((sem = sem_open(clientsemname, O_CREAT | O_RDWR, 0666, 1)) == SEM_FAILED){
-    perror("sem_open");
-    exit(EXIT_FAILURE);
-  }
+  sprintf(servershmname,"/%s_shm:0",serverid);
 
   /***********  Initializing shared memory segment  ***********/
   
@@ -64,46 +112,50 @@ int main(int argc, char ** argv){
     exit(EXIT_FAILURE);
   }
 
-  if(ftruncate(shm_id, sizeof(req_t)) == -1){
+  if(ftruncate(shm_id, sizeof(shm_t)) == -1){
     perror("ftruncate");
     exit(EXIT_FAILURE);
   }
 
-  if((message = mmap(NULL, sizeof(req_t), PROT_READ | PROT_WRITE, MAP_SHARED,
+  if((shm = mmap(NULL, sizeof(shm_t), PROT_READ | PROT_WRITE, MAP_SHARED,
 		     shm_id, 0)) == MAP_FAILED){
     perror("mmap");
     exit(EXIT_FAILURE);
   }
 
-  /*******************  
+  /*****************  Initializing semaphore  *****************/
 
-  /*******************  Infinite loop  *******************/
+  if(sem_init(&(shm->sem), 1, 1) == -1){
+    perror("sem_init");
+    exit(EXIT_FAILURE);
+  }
+
+  /********************  Open server shm  *********************/
+
+  if((server_shm_id = shm_open(servershmname, O_RDWR, 0666)) == -1){
+    perror("shm_open server");
+    exit(EXIT_FAILURE);
+  }
+
+  if((servershm = mmap(NULL, sizeof(req_t), PROT_WRITE, MAP_SHARED, 
+		       server_shm_id, 0)) == MAP_FAILED){
+    perror("mmap server");
+    exit(EXIT_FAILURE);
+  }
+
+  /******************  Changing signal handler  ***************/
+
+  act.sa_handler = sighandler;
+  if((sigaction(SIGINT, &act, NULL) == -1) ||
+     (sigaction(SIGUSR1, &act, NULL) == -1)) {
+    perror("sigaction");
+    exit(EXIT_FAILURE);
+  }
+
+  /*********************  Infinite loop  **********************/
 
   echo_loop();
 
-  /***********  Closing shared memory segment  ***********/
-
-  if(munmap(message, sizeof(req_t)) == -1){
-    perror("munmap");
-    exit(EXIT_FAILURE);
-  }
-
-  if(shm_unlink(clientshmname) == -1){
-    perror("shm_unlink");
-    exit(EXIT_FAILURE);
-  }
-
-  /*****************  Closing semaphore  *****************/
-  
-  if(sem_close(sem) == -1){
-    perror("sem_close");
-    exit(EXIT_FAILURE);
-  }
-
-  if(sem_unlink(clientsemname) == -1){
-    perror("sem_unlink");
-    exit(EXIT_FAILURE);
-  }
 
   return EXIT_SUCCESS;
 }
